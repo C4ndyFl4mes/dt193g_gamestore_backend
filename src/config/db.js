c35a5c3003222d46db1e854c3f3ea5b76b22b7c8
Isaac Svedin <swedinisaac@gmail.com>
2026-01-05 21:00:41 +0100

    Deployment test for Railway,

--- src/config/db.js
+++ src/config/db.js
@@ -7,8 +7,8 @@ const fastifyMysql = require('@fastify/mysql');
 async function mysqlDB(fastify) {
     await fastify.register(fastifyMysql, {
         promise: true,
-        host: "mysql",
-        port: Number(process.env.MYSQL_PORT),
+        host: process.env.MYSQL_HOST,
+        port: Number(process.env.MYSQL_PORT || 3306),
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE

209db53b725156367f7de6cd9add72141ded8193
Isaac Svedin <swedinisaac@gmail.com>
2026-01-05 21:31:54 +0100

    Updated db.js so that Railway may put its values here.

--- src/config/db.js
+++ src/config/db.js
@@ -7,11 +7,11 @@ const fastifyMysql = require('@fastify/mysql');
 async function mysqlDB(fastify) {
     await fastify.register(fastifyMysql, {
         promise: true,
-        host: process.env.MYSQL_HOST,
-        port: Number(process.env.MYSQL_PORT || 3306),
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
+        host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
+        port: Number(process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306),
+        user: process.env.MYSQLUSER || process.env.MYSQL_USER,
+        password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE
     });
 
     fastify.log.info('MySQL Connected!');
